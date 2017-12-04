d1f03597967cd361a856cc2329aff3b9332569ed
tylerchen <iffiff1@gmail.com>
2017-12-04 12:23:46 +0800

    update pom add taobao edas support

--- src/main/java/com/foreveross/common/web/AbstractWsController.java
+++ src/main/java/com/foreveross/common/web/AbstractWsController.java
@@ -50,16 +50,25 @@ import com.foreveross.common.application.Authorization;
  * @since Aug 9, 2015
  * auto generate by qdp.
  */
+@SuppressWarnings("unchecked")
 public abstract class AbstractWsController extends BaseController {
 
 	/**
 	 * <pre>
 	 * map:
 	 *     key   = beanName@@methodName
-	 *     value = List<Method>
+	 *     value = List&lt;Method&gt;
 	 * </pre>
 	 */
 	private static final Map<String, List<Method>> beanMehtodMap = new HashMap<String, List<Method>>();
+	/**
+	 * <pre>
+	 * map:
+	 *     key   = beanName@@AnnotationName
+	 *     value = Authorization
+	 * </pre>
+	 */
+	private static final Map<String, Authorization> beanAnnotationMap = new HashMap<String, Authorization>();
 
 	/**
 	 * invoke bean method and return xml result, only accept "params" parameter name and json value or xstream xml value.
@@ -76,7 +85,6 @@ public abstract class AbstractWsController extends BaseController {
 	public String toJson(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 		try {
-			ActionHelper actionHelper = ActionHelper.create(request, response);
 			String requestURI = request.getRequestURI();
 			String reportPath = StringUtils.substringAfter(requestURI, "/json/");
 			String[] pathSplit = StringUtils.split(reportPath, "/");
@@ -99,11 +107,11 @@ public abstract class AbstractWsController extends BaseController {
 					String tmp = pathSplit[i];
 					String[] tmpSplit = StringUtils.split(tmp, "=");
 					if (tmpSplit.length == 1) {
-						conditionParams.put("arg" + (i - 2), actionHelper.urlDecode(tmpSplit[0]));
+						conditionParams.put("arg" + (i - 2), ActionHelper.urlDecode(tmpSplit[0]));
 					} else if (tmpSplit.length != 2) {
 						continue;
 					} else {
-						conditionParams.put(tmpSplit[0], actionHelper.urlDecode(tmpSplit[1]));
+						conditionParams.put(tmpSplit[0], ActionHelper.urlDecode(tmpSplit[1]));
 					}
 				}
 			}
@@ -119,7 +127,7 @@ public abstract class AbstractWsController extends BaseController {
 			return value;
 		} catch (Exception ex) {
 			return GsonHelper.toJsonString(
-					ResultBean.error().setBody(FCS.get("get json error: {0}", ex.getStackTrace()).toString()));
+					ResultBean.error().setBody(FCS.get("get json error: {0}", ex.getMessage()).toString()));
 		}
 	}
 
@@ -138,7 +146,6 @@ public abstract class AbstractWsController extends BaseController {
 	public String toXml(ModelMap modelMap, HttpServletRequest request, HttpServletResponse response)
 			throws IOException {
 		try {
-			ActionHelper actionHelper = ActionHelper.create(request, response);
 			String requestURI = request.getRequestURI();
 			String reportPath = StringUtils.substringAfter(requestURI, "/xml/");
 			String[] pathSplit = StringUtils.split(reportPath, "/");
@@ -161,11 +168,11 @@ public abstract class AbstractWsController extends BaseController {
 					String tmp = pathSplit[i];
 					String[] tmpSplit = StringUtils.split(tmp, "=");
 					if (tmpSplit.length == 1) {
-						conditionParams.put("arg" + (i - 2), actionHelper.urlDecode(tmpSplit[0]));
+						conditionParams.put("arg" + (i - 2), ActionHelper.urlDecode(tmpSplit[0]));
 					} else if (tmpSplit.length != 2) {
 						continue;
 					} else {
-						conditionParams.put(tmpSplit[0], actionHelper.urlDecode(tmpSplit[1]));
+						conditionParams.put(tmpSplit[0], ActionHelper.urlDecode(tmpSplit[1]));
 					}
 				}
 			}
@@ -175,7 +182,7 @@ public abstract class AbstractWsController extends BaseController {
 			return invokeToString(beanName, methodName, bean, conditionParams, XStreamHelper.class);
 		} catch (Exception ex) {
 			return GsonHelper.toJsonString(
-					ResultBean.error().setBody(FCS.get("get xml error: {0}", ex.getStackTrace()).toString()));
+					ResultBean.error().setBody(FCS.get("get xml error: {0}", ex.getMessage()).toString()));
 		}
 	}
 
@@ -263,47 +270,68 @@ public abstract class AbstractWsController extends BaseController {
 
 	private List<Method> findMethod(String beanName, String methodName, Object bean) {
 		String beanMethodKey = beanMethodKey(beanName, methodName);
-		beanMehtodMap.clear();
 		List<Method> list = beanMehtodMap.get(beanMethodKey);
-		if (list != null) {
-			return list;
-		}
-		{//授权判断 //TODO 没有做会话级【角色】的判断，如果要做会话级的判断，这一段前置几行代码。
-			Authorization authorization = SpringContextHelper.getApplicationContext().findAnnotationOnBean(beanName,
-					Authorization.class);
-			if (!canAccessBean(authorization, beanName, getRoles())) {
-				return list;
+		if (list == null || list.isEmpty()) {
+			list = new ArrayList<Method>();
+			List<Class<?>> clazzList = new ArrayList<Class<?>>();
+			try {
+				ApplicationContext applicationContext = SpringContextHelper.getApplicationContext();
+				ConfigurableApplicationContext xml = (ConfigurableApplicationContext) applicationContext;
+				DefaultListableBeanFactory bf = (DefaultListableBeanFactory) xml.getBeanFactory();
+				Class<?> loadClass = bf.getType(beanName);
+				if (loadClass != null) {
+					String className = null;
+					className = (String) bf.getBeanDefinition(beanName).getPropertyValues().get("interfaceName");
+					if (StringUtils.isBlank(className)) {
+						className = bf.getBeanDefinition(beanName).getBeanClassName();
+					}
+					loadClass = xml.getClassLoader().loadClass(className);
+				}
+				if (loadClass.isInterface()) {//如果只是代理类，一般就是接口
+					clazzList.add(loadClass);
+				} else {//如果是实现类，一般要拿到其接口
+					Class<?>[] interfaces = loadClass.getInterfaces();
+					clazzList.addAll(Arrays.asList(interfaces));
+				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		}
-		list = new ArrayList<Method>();
-		List<Class<?>> clazzList = new ArrayList<Class<?>>();
-		try {
-			ApplicationContext applicationContext = SpringContextHelper.getApplicationContext();
-			ConfigurableApplicationContext xml = (ConfigurableApplicationContext) applicationContext;
-			DefaultListableBeanFactory bf = (DefaultListableBeanFactory) xml.getBeanFactory();
-			String className = bf.getBeanDefinition(beanName).getBeanClassName();
-			Class<?> loadClass = xml.getClassLoader().loadClass(className);
-			Class<?>[] interfaces = loadClass.getInterfaces();
-			clazzList.addAll(Arrays.asList(interfaces));
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		Map<String, Boolean> map = new HashMap<String, Boolean>();
-		{
-			for (Class<?> cls : clazzList) {
-				if (cls.getName().endsWith("Application")) {
-					Method[] methods = cls.getDeclaredMethods();
-					for (Method m : methods) {
-						map.put(m.getName(), Boolean.TRUE);
-						if (methodName.equals(m.getName()) && Modifier.isPublic(m.getModifiers())) {
-							list.add(m);
+			Map<String, Boolean> map = new HashMap<String, Boolean>();
+			{
+				for (Class<?> cls : clazzList) {
+					if (cls.getName().endsWith("Application")) {
+						Method[] methods = cls.getDeclaredMethods();
+						for (Method m : methods) {
+							map.put(m.getName(), Boolean.TRUE);
+							if (methodName.equals(m.getName()) && Modifier.isPublic(m.getModifiers())) {
+								list.add(m);
+							}
 						}
 					}
 				}
 			}
+			synchronized (beanMehtodMap) {
+				beanMehtodMap.put(beanMethodKey, list);
+			}
 		}
-		synchronized (beanMehtodMap) {
-			beanMehtodMap.put(beanMethodKey, list);
+		{//授权判断 //TODO 没有做会话级【角色】的判断，如果要做会话级的判断，这一段前置几行代码。
+			String beanAnnoKey = beanName + "@@" + Authorization.class.getName();
+			Authorization authorization = beanAnnotationMap.get(beanAnnoKey);
+			if (authorization != null) {
+				if (!canAccessBean(authorization, beanName, getRoles())) {
+					return Collections.EMPTY_LIST;
+				}
+			} else {
+				//查找类上定义的注解,包括父类与接口
+				authorization = SpringContextHelper.getApplicationContext().findAnnotationOnBean(beanName,
+						Authorization.class);
+				if (authorization != null) {
+					beanAnnotationMap.put(beanAnnoKey, authorization);
+				}
+				if (!canAccessBean(authorization, beanName, getRoles())) {
+					return Collections.EMPTY_LIST;
+				}
+			}
 		}
 		return list;
 	}
